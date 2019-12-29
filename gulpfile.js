7fd2b63170c03141baf47db1e5af9cf1717fcd1a
Михаил Маклай <mikhailmaclay@gmail.com>
2019-12-29 15:17:19 +0300

    Добавляет к проекту gulp-svg-sprite

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,20 @@ var sass = require("gulp-sass");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var svgsprite = require('gulp-svg-sprite');
+
+gulp.task("svgsprite", function () {
+  return gulp.src("source/img/svg/*.svg")
+    .pipe(svgsprite({
+        mode: {
+          stack: {
+            sprite: "../sprite.svg"
+          }
+        },
+      }
+    ))
+    .pipe(gulp.dest('source/img/'));
+});
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
