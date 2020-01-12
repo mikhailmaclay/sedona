55db33ddc1ee399d25cdc3f99c81b5647ef6e5ff
Михаил Маклай <mikhailmaclay@gmail.com>
2020-01-12 20:11:16 +0300

    Оптимизирует изображения, добавляет webp, добавляет новые модули, добавляет скрипты для сборки проекта

--- source/js/script.js
+++ source/js/script.js
@@ -1,13 +1,1 @@
-// Полифилы
-import './polyfills/picturefill.js';
-import './polyfills/svguse.js';
-
-// Помощники
-import './utils/browserDetection.js';
-import './utils/no-js-clear.js';
-
-// Компоненты
-import './components/formReview.js';
-import './components/overlay.js';
-import './components/modal.js';
-import './components/navigation.js';
+'use strict';
